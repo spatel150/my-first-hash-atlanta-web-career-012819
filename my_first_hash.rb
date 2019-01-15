df58dd80d210a031d86800df318275a261807d22
Samidh Patel <spatel13456@gmail.com>
2019-01-15 03:01:38 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -10,7 +10,8 @@ def shipping_manifest
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
   the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  return the_manifest  
+  return the_manifest
+  end
 end
 
 def retrieval
@@ -23,6 +24,7 @@ def retrieval
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
   shipping_manifest["oil paintings"]
   return shipping_manifest
+  end
 end
 
 def adding
@@ -41,4 +43,5 @@ def adding
 
   # return the shipping_manifest hash below
   return shipping_manifest
+  end
 end
